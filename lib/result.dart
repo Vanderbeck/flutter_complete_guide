ef37b91f2879b7e35042bbaaad5b511b704c37ee
Lindsay Vanderbeck <lindsay.vanderbeck@live.ca>
2020-03-15 12:05:21 -0400

    result now accepts list from answerQuestion in main to show answers.

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final List resultList;
+
+  Result({this.resultList});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('YOU DID IT!'),
-    );
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Container(
+        width: double.infinity,
+        child: Text('YOU DID IT!', textAlign: TextAlign.center),
+      ),
+      Container(
+        width: double.infinity,
+        child: Text(resultList.join("\n"), textAlign: TextAlign.center),
+      )
+    ]);
   }
 }
